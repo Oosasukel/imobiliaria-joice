c77b7382e3e334bd6333c5246cfc5b8f55f8cae2
Rodrigo Gonçalves Do Nascimento <rodrigo.nascimento@dasa.com.br>
2022-10-23 11:44:40 -0300

    firebase storage, createCollections e formidable

---
+++ README.md
@@ -0,0 +1,17 @@
+# Como rodar o projeto localmente
+
+## Configure as variáveis de ambiente
+
+Crie um arquivo `.env` e configure conforme o `.env.example`
+
+## Crie as collections e indexes no banco de dados
+
+`yarn createCollections`
+
+## Instale as dependências
+
+`yarn`
+
+## Execute
+
+`yarn dev`
--- src/pages/api/storage/index.ts
+++ src/pages/api/storage/index.ts
@@ -4,18 +4,36 @@ import { defaultOptions } from '../../../api/nextConnect/defaultOptions';
 
 const handler = nc<NextApiRequest, NextApiResponse>(defaultOptions);
 
-handler.get(async (req, res) => {
-  try {
-    // const fileRef = ref(storage, 'images/test.jpg');
-
-    // const imageUrl = await getDownloadURL(fileRef);
-
-    // const snapshot = await uploadBytes(fileRef, file);
-
-    return res.json('123');
-  } catch (error) {
-    return res.json(error);
-  }
+handler.post(async (req, res) => {
+  // -------------------------------------- POST
+  // const form = formidable({ multiples: true });
+  // form.parse(req, async (err, fields, files) => {
+  //   if (err) {
+  //     return res.status(400).send('Error parsing files');
+  //   }
+  //   const mountainsRef = ref(storage, 'images/mountains.jpg');
+  //   const myFile = await fs.readFile((files.image as any).filepath as string);
+  //   console.log(myFile);
+  //   const snapshot = await uploadBytes(mountainsRef, myFile, {
+  //     contentType: (files.image as any).mimetype as string,
+  //   });
+  //   console.log('snapshot', snapshot);
+  //   return res.json({ fields, files });
+  // });
+  // -------------------------------------- GET
+  // try {
+  //   const fileRef = ref(storage, 'goku.jpg');
+  //   const imageUrl = await getDownloadURL(fileRef);
+  //   return res.json({ imageUrl });
+  // } catch (error) {
+  //   return res.json(error);
+  // }
 });
 
 export default handler;
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
