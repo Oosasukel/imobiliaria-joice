2f32112fc64a7e9a61d6fafe704d304c7d882b6f
Rodrigo Gonçalves <rodrigo_gonn@hotmail.com>
2022-12-29 15:41:18 -0300

    removing initial props

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,7 @@
-import { NextPageContext } from 'next';
 import { AppProps as NextAppProps } from 'next/app';
 import Head from 'next/head';
 import { Configurations, Enums } from '../hooks/useApi/types';
 import { AppProvider } from '../hooks/useAppState';
-import { api } from '../services/api';
 import { GlobalCSS } from '../styles/global';
 
 interface AppProps extends NextAppProps {
@@ -12,15 +10,13 @@ interface AppProps extends NextAppProps {
   cities: string[];
 }
 
-const CustomApp = ({
-  Component,
-  pageProps,
-  configs,
-  enums,
-  cities,
-}: AppProps) => {
+const CustomApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <AppProvider configs={configs} enums={enums} cities={cities}>
+    <AppProvider
+      configs={{} as Configurations}
+      enums={{ status: [], types: [] }}
+      cities={[]}
+    >
       <Head>
         <title>Imobiliária Joice</title>
       </Head>
@@ -29,34 +25,6 @@ const CustomApp = ({
       <Component {...pageProps} />
     </AppProvider>
   );
-};
-
-CustomApp.getInitialProps = async ({
-  Component,
-  ctx,
-}: {
-  Component: any;
-  ctx: NextPageContext;
-}) => {
-  let pageProps = {};
-  let configs: Configurations;
-  let enums: Enums;
-  let cities: string[];
-
-  if (Component.getInitialProps) {
-    pageProps = await Component.getInitialProps(ctx);
-  }
-
-  const { data: configsResponse } = await api.get<Configurations>(
-    '/api/configurations'
-  );
-  const { data: enumsResponse } = await api.get<Enums>('/api/enums');
-  const { data: citiesResponse } = await api.get<string[]>('/api/cities');
-  configs = configsResponse;
-  enums = enumsResponse;
-  cities = citiesResponse;
-
-  return { ...pageProps, configs, enums, cities } as AppProps;
 };
 
 export default CustomApp;
