fbbf3df6d23de1c4209112dc5c173bda3bc73a53
Rodrigo Gonçalves <rodrigo_gonn@hotmail.com>
2022-12-29 15:45:23 -0300

    phoneNumber null

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -10,15 +10,19 @@ export const Footer = () => {
   return (
     <Container>
       <Content>
-        <span className="contato">Contato</span>
-        <div className="whatsapp">
-          <img src="/icons/whatsapp.svg" alt="icone do whatsapp" />
-          <span>
-            {configs.phoneNumber
-              .replace(/\D+/g, '')
-              .replace(/(\d{2})(\d{2})(\d{5})(\d{4})/, '($2) $3-$4')}
-          </span>
-        </div>
+        {configs.phoneNumber && (
+          <>
+            <span className="contato">Contato</span>
+            <div className="whatsapp">
+              <img src="/icons/whatsapp.svg" alt="icone do whatsapp" />
+              <span>
+                {configs.phoneNumber
+                  .replace(/\D+/g, '')
+                  .replace(/(\d{2})(\d{2})(\d{5})(\d{4})/, '($2) $3-$4')}
+              </span>
+            </div>
+          </>
+        )}
         <p className="copyrigth">
           ©2022 Copyright - Imobiliária Joice Decicillo | CRECI {configs.creci}{' '}
           | Todos os direitos reservados
